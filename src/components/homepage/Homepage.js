eb33e238cf93f76198ab2589476b31b71870f0dd
Spencer Knight <spencer.knightusn@gmail.com>
2022-03-16 18:43:20 -0700

    added reset css / react router

---
+++ src/components/homepage/Homepage.js
@@ -0,0 +1,9 @@
+import { useEffect } from 'react';
+import { Link, Outlet } from 'react-router-dom';
+import './homepage.css';
+
+function Homepage() {
+  return <div>homepage</div>;
+}
+
+export default Homepage;
--- src/index.js
+++ src/index.js
@@ -1,12 +1,17 @@
+import './reset.css';
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
 import reportWebVitals from './reportWebVitals';
+import Homepage from './components/homepage/Homepage';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<Homepage />} />
+      </Routes>
+    </BrowserRouter>
   </React.StrictMode>,
   document.getElementById('root')
 );
