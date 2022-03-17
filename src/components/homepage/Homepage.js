c0117cecaac9c37fa2aefef08531cd6fc153a284
Spencer Knight <spencer.knightusn@gmail.com>
2022-03-17 11:29:38 -0700

    added reveal

--- src/components/homepage/Homepage.js
+++ src/components/homepage/Homepage.js
@@ -1,10 +1,10 @@
+import './homepage.css';
+import { gsap } from 'gsap';
+import { useEffect } from 'react';
+import { ScrollTrigger } from 'gsap/ScrollTrigger';
 import LandingPage from './pages/landingPage/LandingPage';
 import Placeholder from './pages/placeholder1/Placeholder';
 import Placeholder2 from './pages/placeholder2/Placeholder2';
-import { gsap } from 'gsap';
-import { ScrollTrigger } from 'gsap/ScrollTrigger';
-import { useEffect } from 'react';
-import './homepage.css';
 
 function Homepage() {
   useEffect(() => {
@@ -14,18 +14,24 @@ function Homepage() {
         trigger: panel,
         start: 'top top',
         pin: true,
-        pinSpacing: false,
+        pinSpacing: true, // gives extra space for each page
       });
     });
-
-    ScrollTrigger.create({
-      snap: 1 / 4, // snap whole page to the closest section!
+    //
+    gsap.to('.firstBox', {
+      opacity: 1,
+      scrollTrigger: {
+        trigger: '.placeholder',
+        start: 'top top',
+        end: '+=500',
+        scrub: true,
+      },
     });
   }, []);
 
   return (
     <>
-      <main>
+      <main className='container'>
         <LandingPage />
         <Placeholder />
         <Placeholder2 />
--- src/components/homepage/pages/placeholder1/Placeholder.js
+++ src/components/homepage/pages/placeholder1/Placeholder.js
@@ -1,7 +1,12 @@
 import './placeholder.css';
 
 function Placeholder() {
-  return <div className='placeholder page'>placeholder</div>;
+  return (
+    <div className='placeholder page'>
+      <div className='firstBox'>first</div>
+      <div className='secondBox'>second</div>
+    </div>
+  );
 }
 
 export default Placeholder;
