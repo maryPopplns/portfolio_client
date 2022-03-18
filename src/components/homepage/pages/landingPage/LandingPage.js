f5d7a4d0a0362d5f4ba1ab7e765033456c1c369f
Spencer Knight <spencer.knightusn@gmail.com>
2022-03-18 07:29:46 -0700

    added archivo font

--- src/components/homepage/pages/landingPage/LandingPage.js
+++ src/components/homepage/pages/landingPage/LandingPage.js
@@ -3,8 +3,8 @@ import './landingPage.css';
 function LandingPage() {
   return (
     <main className='landingPage page'>
-      <h1 className='first_name_heading'>SPENCER</h1>
-      <h1 className='last_name_heading'>KNIGHT</h1>
+      <h1 className='name_heading first_name_heading'>SPENCER</h1>
+      <h1 className='name_heading last_name_heading'>KNIGHT</h1>
     </main>
   );
 }
