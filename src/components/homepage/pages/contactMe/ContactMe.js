d73742f20c4ccb9a5796299dae782bc2f70d3113
Spencer Knight <spencer.knightusn@gmail.com>
2022-03-21 10:44:49 -0700

    cleaned up css

--- src/components/homepage/pages/contactMe/ContactMe.js
+++ src/components/homepage/pages/contactMe/ContactMe.js
@@ -11,48 +11,50 @@ function ContactMe() {
     <main className='contact page'>
       <h1>contact</h1>
       <hr id='header_contact_divisor'></hr>
-      <form>
-        <div className='contact_input_container'>
-          <label htmlFor='email'>email</label>
-          <input
-            onChange={({ target }) => setEmail(target.value)}
-            value={email}
-            type='email'
-            id='email'
-            name='email'
-            required
-          ></input>
+      <div className='contact_form_container'>
+        <form>
+          <div className='contact_input_container'>
+            <label htmlFor='email'>email</label>
+            <input
+              onChange={({ target }) => setEmail(target.value)}
+              value={email}
+              type='email'
+              id='email'
+              name='email'
+              required
+            ></input>
+          </div>
+          <div className='contact_input_container'>
+            <label htmlFor='message'>message</label>
+            <textarea
+              onChange={({ target }) => setMessage(target.value)}
+              value={message}
+              type='textarea'
+              id='message'
+              name='message'
+              required
+            ></textarea>
+          </div>
+          <div className='submit_contact_container'>
+            <button>submit</button>
+          </div>
+        </form>
+        <div className='contact_form_icons'>
+          <a
+            href='https://www.linkedin.com/in/spencer-knight-dev/'
+            target='_blank'
+            rel='noreferrer'
+          >
+            <img src={linkedin} alt='linkedin icon'></img>
+          </a>
+          <a
+            href='https://github.com/maryPopplns'
+            target='_blank'
+            rel='noreferrer'
+          >
+            <img src={github} alt='github icon'></img>
+          </a>
         </div>
-        <div className='contact_input_container'>
-          <label htmlFor='message'>message</label>
-          <textarea
-            onChange={({ target }) => setMessage(target.value)}
-            value={message}
-            type='textarea'
-            id='message'
-            name='message'
-            required
-          ></textarea>
-        </div>
-        <div className='submit_contact_container'>
-          <button>submit</button>
-        </div>
-      </form>
-      <div className='contact_form_icons'>
-        <a
-          href='https://www.linkedin.com/in/spencer-knight-dev/'
-          target='_blank'
-          rel='noreferrer'
-        >
-          <img src={linkedin} alt='linkedin icon'></img>
-        </a>
-        <a
-          href='https://github.com/maryPopplns'
-          target='_blank'
-          rel='noreferrer'
-        >
-          <img src={github} alt='github icon'></img>
-        </a>
       </div>
     </main>
   );
