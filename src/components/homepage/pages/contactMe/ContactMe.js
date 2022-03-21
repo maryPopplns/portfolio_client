100e7ad65f38d09b86467c69513aa6148be2542a
Spencer Knight <spencer.knightusn@gmail.com>
2022-03-21 07:19:13 -0700

    added message inputs

--- src/components/homepage/pages/contactMe/ContactMe.js
+++ src/components/homepage/pages/contactMe/ContactMe.js
@@ -1,13 +1,31 @@
+import { useState } from 'react';
 import './contactMe.css';
 
 function ContactMe() {
+  const [title, setTitle] = useState('');
+  const [message, setMessage] = useState('');
+
   return (
     <main className='contact page'>
       <h1>contact</h1>
       <hr id='header_contact_divisor'></hr>
       <form>
-        <label></label>
-        <input></input>
+        <label htmlFor='title'></label>
+        <input
+          onChange={({ target }) => setTitle(target.value)}
+          value={title}
+          type='text'
+          id='title'
+          name='title'
+        ></input>
+        <label htmlFor='message'></label>
+        <textarea
+          onChange={({ target }) => setMessage(target.value)}
+          value={message}
+          type='textarea'
+          id='message'
+          name='message'
+        ></textarea>
       </form>
     </main>
   );
