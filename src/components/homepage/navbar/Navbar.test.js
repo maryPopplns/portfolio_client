236d5f1dafd67ff2447feb474f85a9f7e46e1fd4
Spencer Knight <spencer.knightusn@gmail.com>
2022-03-24 21:54:48 -0700

    navbar testing complete

--- src/components/homepage/navbar/Navbar.test.js
+++ src/components/homepage/navbar/Navbar.test.js
@@ -1,25 +1,59 @@
 import { render, screen, waitFor } from '@testing-library/react';
 import { MemoryRouter } from 'react-router-dom';
-// import userEvent from '@testing-library/user-event';
+import userEvent from '@testing-library/user-event';
 import Navbar from './Navbar';
 
+function setup() {
+  render(
+    <MemoryRouter>
+      <Navbar />
+    </MemoryRouter>
+  );
+}
+
 describe('navbar', () => {
-  test('blog link', () => {
-    render(
-      <MemoryRouter>
-        <Navbar />
-      </MemoryRouter>
-    );
-    const blogLink = screen.getByRole('link', { name: /blog/i });
-    expect(blogLink).toBeInTheDocument();
+  describe('button text', () => {
+    test('blog link', () => {
+      setup();
+      const blogLink = screen.getByRole('link', { name: /blog/i });
+      expect(blogLink).toBeInTheDocument();
+    });
+    test('projects link', () => {
+      setup();
+      const projectsLink = screen.getByRole('link', { name: /projects/i });
+      expect(projectsLink).toBeInTheDocument();
+    });
   });
-  test('projects link', () => {
-    render(
-      <MemoryRouter>
-        <Navbar />
-      </MemoryRouter>
-    );
-    const projectsLink = screen.getByRole('link', { name: /projects/i });
-    expect(projectsLink).toBeInTheDocument();
+  describe('blog hover effects', () => {
+    test('adds hovered class when hovered', () => {
+      setup();
+      const blogLink = screen.getByRole('link', { name: /blog/i });
+      userEvent.hover(blogLink);
+      expect(blogLink).toHaveClass('navbarHovered');
+    });
+    test('should remove hovered class when unhovered', () => {
+      setup();
+      const blogLink = screen.getByRole('link', { name: /blog/i });
+      userEvent.hover(blogLink);
+      expect(blogLink).toHaveClass('navbarHovered');
+      userEvent.unhover(blogLink);
+      expect(blogLink).not.toHaveClass('navbarHovered');
+    });
+  });
+  describe('projects hover effects', () => {
+    test('adds hovered class when hovered', () => {
+      setup();
+      const projectsLink = screen.getByRole('link', { name: /projects/i });
+      userEvent.hover(projectsLink);
+      expect(projectsLink).toHaveClass('navbarHovered');
+    });
+    test('should remove hovered class when unhovered', () => {
+      setup();
+      const projectsLink = screen.getByRole('link', { name: /projects/i });
+      userEvent.hover(projectsLink);
+      expect(projectsLink).toHaveClass('navbarHovered');
+      userEvent.unhover(projectsLink);
+      expect(projectsLink).not.toHaveClass('navbarHovered');
+    });
   });
 });
