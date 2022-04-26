5fc52711c8174c22e297500181a5a40f095b1217
Spencer Knight <spencer.knightusn@gmail.com>
2022-04-26 16:00:09 -0700

    added test for home link

--- src/components/navbar/Navbar.test.js
+++ src/components/navbar/Navbar.test.js
@@ -1,21 +1,56 @@
+import { rest } from 'msw';
 import Navbar from './Navbar';
-import { Provider } from 'react-redux';
+import Blog from '../blog/Blog';
 import store from '../../store/store';
+import { setupServer } from 'msw/node';
+import { Provider } from 'react-redux';
+import Homepage from '../homepage/Homepage';
 import { MemoryRouter } from 'react-router-dom';
+import { Routes, Route } from 'react-router-dom';
 import userEvent from '@testing-library/user-event';
 import { render, screen, waitFor } from '@testing-library/react';
+
+const response = [
+  {
+    _id: '62670a947415fdf9241a6632',
+    showing: false,
+    date: '2022-04-25T20:54:44.490Z',
+    title: 'test title',
+    body: 'the body',
+    category: 'test',
+    comments: [],
+  },
+];
 
 function setup() {
   render(
     <Provider store={store}>
       <MemoryRouter>
-        <Navbar />
+        <Routes>
+          <Route path='/' element={<Navbar />}>
+            <Route index element={<Homepage />} />
+            <Route path='blog' element={<Blog />} />
+          </Route>
+        </Routes>
       </MemoryRouter>
     </Provider>
   );
 }
 
 describe('navbar', () => {
+  const contactMeResponse = rest.get(
+    'https://whispering-depths-29284.herokuapp.com/post',
+    (req, res, ctx) => {
+      return res(ctx.json(response));
+    }
+  );
+
+  const server = new setupServer(contactMeResponse);
+
+  beforeAll(() => server.listen());
+  afterEach(() => server.resetHandlers());
+  afterAll(() => server.close());
+
   describe('button text', () => {
     test('blog link', () => {
       setup();
@@ -58,6 +93,36 @@ describe('navbar', () => {
       expect(projectsLink).toHaveClass('navbarHovered');
       userEvent.unhover(projectsLink);
       expect(projectsLink).not.toHaveClass('navbarHovered');
+    });
+  });
+  describe('home hover effects', () => {
+    test('adds hovered class when hovered', async () => {
+      setup();
+      const blogLink = screen.getByRole('link', { name: /blog/i });
+      userEvent.click(blogLink);
+
+      const homeLink = screen.getByRole('link', { name: 'home' });
+      expect(homeLink).toBeInTheDocument();
+      userEvent.hover(homeLink);
+      expect(homeLink).toHaveClass('navbarHovered');
+    });
+    test('should remove hovered class when unhovered', () => {
+      setup();
+      const blogLink = screen.getByRole('link', { name: /blog/i });
+      userEvent.click(blogLink);
+
+      const homeLink = screen.getByRole('link', { name: 'home' });
+      userEvent.hover(homeLink);
+      expect(homeLink).toHaveClass('navbarHovered');
+      userEvent.unhover(homeLink);
+      expect(homeLink).not.toHaveClass('navbarHovered');
+    });
+  });
+  describe('data', () => {
+    test.skip('pulls data from api', () => {
+      setup();
+      // TODO finish test once blog component is complete
+      // just load component, click blog link ,data is loaded at top of file, then just assert that the title is present
     });
   });
 });
